f17feb76ac4ceba8a373d140acba8e71ade7d712
ParNurZeal <parnurzeal@gmail.com>
2013-06-01 18:00:48 +0900

    change protocal format, seperate filter into .json file, parameter UI now reflected to receiveing protocol from server

--- node-server.js
+++ node-server.js
@@ -4,11 +4,14 @@ var WebSocketServer = require('websocket').server;
 var http=require('http');
 var fs=require('fs');
 
+var filters_json = fs.readFileSync('filters.json','utf8');
+
 var server = http.createServer(function(request, response){
   console.log((new Date()) + ' Received request for ' + request.url);
   response.writeHead(404);
   response.end();
 });
+
 server.listen(8080,function() {
   console.log((new Date()) + ' Server is listening on port 8080');
 });
@@ -20,19 +23,17 @@ wsServer = new WebSocketServer({
 wsServer.on('request', function(request){
   var connection = request.accept(null,request.origin);
   console.log((new Date()) + ' Connection accepted.');
+  // send filters' format to client
+  console.log("sending \n: " + filters_json);
+  connection.send(filters_json);
+
   connection.on('message', function(message){
     try{
       var json_obj = JSON.parse(message.utf8Data);
       console.log('Received json message:' + json_obj);
-      if(json_obj.interface_type==='hough'){
+      if(json_obj.selected_filter.name==='Canny'){
         fs.readFile("test.png",'base64', function(err, data){
-          json_send_data = {
-                        "image": data,
-                        "filter":{
-                          "name":"hough",
-                          "parameters": [ {"name":"theta","type":"double"}, {"name":"beta","type":"int"}]
-                        } 
-                      };
+          json_send_data = { "image": data};
           console.log(json_send_data);
           connection.send(JSON.stringify(json_send_data));
         });
@@ -40,6 +41,7 @@ wsServer.on('request', function(request){
         console.log('wrong interface_type');
       }
     }catch(e){
+      console.log(e);
       if(message.type==='utf8'){
         console.log('Received non-json message: ' + message.utf8Data);
       }else if(message.type==='binary'){
